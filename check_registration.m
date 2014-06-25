896d3e64bf688b82d4c8eacd3504105474178c06
Walid <theinterwebnets@gmail.com>
2014-06-25 14:52:21 -0400

    check_registration edit
    now can take functional run integer input (default = 1)

--- check_registration.m
+++ check_registration.m
@@ -1,11 +1,13 @@
-function check_registration(EXPT,subj)
+function check_registration(EXPT,subj,func)
     
     % Check registration of anatomical and functionals.
     %
     % USAGE: check_registration(EXPT,subj)
-    
+    % ALT USAGE: check_registration(EXPT,subj,functionalRun)
+	
+    if nargin < 3, func = 1; end
     S = EXPT.subject(subj);
-    niftidir = S.functional(1).niftidir;
+    niftidir = S.functional(func).niftidir;
     P{1} = fmri_get(fullfile(niftidir,'wf*'));
     P{1} = P{1}(1,:);
     run = S.anatomical.run;
