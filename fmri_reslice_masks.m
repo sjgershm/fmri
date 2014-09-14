9c487512cb78c79667f832a7a2de921b20b8978c
samgershman <sam.gershman@gmail.com>
2014-09-13 21:27:32 -0400

    fixed bug

--- fmri_reslice_masks.m
+++ fmri_reslice_masks.m
@@ -1,24 +1,21 @@
-function masks = fmri_reslice_masks(EXPT,subj,maskdir)
+function masks = fmri_reslice_masks(EXPT,subj,model,maskdir)
     
     S = EXPT.subject(subj);
     run = S.anatomical.run;
     anatomical = fmri_get(fullfile(S.anatomical.niftidir,sprintf('*-%3.4d-*',run)));
     
-    F = dir(fullfile(maskdir,'*.nii'));
-    for i = 1:length(F)
-        P{i,1} = fullfile(maskdir,F(i).name);
-    end
-    P = [anatomical; P];
-    
-    spm_reslice(P);
-    
     % load SPM mask
+    M = ['model',num2str(model)];
     V = spm_vol(fullfile(EXPT.analysis_dir,S.name,M,'mask.img'));
     mask = spm_read_vols(V); mask = mask~=0;
     
-    F = dir(fullfile(maskdir,'r*.nii'));
+    F = dir(fullfile(maskdir,'*.nii'));
+    masks = cell(1,length(F));
     for i = 1:length(F)
-        f = fullfile(maskdir,F(i).name);
+        disp(F(i).name);
+        P = fullfile(maskdir,F(i).name);
+        spm_reslice([anatomical; P]);
+        f = fullfile(maskdir,['r',F(i).name]);
         V = spm_vol(f);
         Y = spm_read_vols(V);
         masks{i} = Y(mask)>0.5;
