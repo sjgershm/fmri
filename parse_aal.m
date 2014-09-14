37605d4b9412d9836ee30fc43d9466f6dc4ce6c9
samgershman <sam.gershman@gmail.com>
2014-09-13 22:02:26 -0400

    update

--- parse_aal.m
+++ parse_aal.m
@@ -1,7 +1,7 @@
 function names = parse_aal
     
-    spmdir = fileparts(which('SPM.m'));
-    T = fullfile(spmdir,'toolbox/WFU_PickAtlas_3.0.4/wfu_pickatlas/MNI_atlas_templates/aal_MNI_V4.txt');
+    fdir = fileparts(which('aal_MNI_V4.txt'));
+    T = fullfile(fdir,'aal_MNI_V4.txt');
     f = fopen(T);
     fgetl(f);   % remove header
     while ~feof(f)
