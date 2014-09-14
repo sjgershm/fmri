bed155e886ddca6b139b35b697d171c4b5384b5f
samgershman <sam.gershman@gmail.com>
2014-09-13 21:56:20 -0400

    added function to parse AAL labels into cell array

---
+++ parse_aal.m
@@ -0,0 +1,14 @@
+function names = parse_aal
+    
+    spmdir = which('SPM.m');
+    T = fullfile(spmdir,'toolbox/WFU_PickAtlas_3.0.4/wfu_pickatlas/MNI_atlas_templates/aal_MNI_V4.txt');
+    f = fopen(T);
+    fgetl(f);   % remove header
+    while ~feof(f)
+        L = fgetl(f);
+        words = regexp(L,'\t','split');
+        n = str2num(words{1});
+        names{n,1} = words{2};
+    end
+    
+    fclose(f);
