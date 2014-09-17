69beb95c793297c541253d45dd1219ae0cb1e7aa
samgershman <sam.gershman@gmail.com>
2014-09-17 16:42:14 -0400

    updated load beta function

--- fmri_load_beta.m
+++ fmri_load_beta.m
@@ -11,45 +11,49 @@ function [beta, mask] = fmri_load_beta(EXPT,model,subj,names)
     %   names - [N x 1] cell array of event names
     %
     % OUTPUTS:
-    %   beta - [N x 1] cell array, where each cell contains a [K x V] matrix of 
+    %   beta - [N x 1] cell array, where each cell contains a [K x V] matrix of
     %          beta images; K refers to the number of regressors matching
-    %          the event name, and V refers to the number of voxels in the mask
+    %          the event names, and V refers to the number of voxels in the mask
     %          image automatically created by SPM
     %   mask - binary image indicating which voxels are included in the mask
     %
     % Sam Gershman, June 2014
-	% Updates:
-	%	Walid 2014-08-26: Now can take 'names' as a numeric array to bypass the
-	%					  find_regressors step
-	
+    % Updates:
+    %	Walid 2014-08-26: Now can take 'names' as a numeric array to bypass the
+    %					  find_regressors step
+    
     S = EXPT.subject(subj);
     M = ['model',num2str(model)];
-    load(fullfile(EXPT.analysis_dir,S.name,M,'SPM'));
     
     % load SPM mask
-    V = spm_vol(fullfile(EXPT.analysis_dir,S.name,M,'mask.img'));
+    V = spm_vol(fullfile(EXPT.analysis_dir,S.names,M,'run1','mask.img'));
     mask = spm_read_vols(V); mask = mask~=0;
     
-	if islogical(names)
-        beta = [];
-		names = find(names);
-		for j = 1:length(names)
-			fname = sprintf('beta_%3.4d.img',names(j));
-			V = spm_vol(fullfile(EXPT.analysis_dir,S.name,M,fname));
-			Y = spm_read_vols(V);
-			beta(j,:) = Y(mask);
-		end
-    else
-        beta = cell(length(names),1);
-        for i = 1:length(names)
-            c = find_regressors(SPM.xX.name',names{i});
-            c = find(c);
-            disp(names{i});
-            for j = 1:length(c)
-                fname = sprintf('beta_%3.4d.img',c(j));
-                V = spm_vol(fullfile(EXPT.analysis_dir,S.name,M,fname));
+    ii = 0;
+    for run = 1:length(S.functional)
+        load(fullfile(EXPT.analysis_dir,S.names,M,['run',num2str(run)],'regnames'));
+        if islogical(names)
+            beta = [];
+            names = find(names);
+            for j = 1:length(names)
+                fname = sprintf('beta_%3.4d.img',names(j));
+                V = spm_vol(fullfile(EXPT.analysis_dir,S.names,M,fname));
                 Y = spm_read_vols(V);
-                beta{i}(j,:) = Y(mask);
+                beta(j,:) = Y(mask);
+            end
+        else
+            beta = cell(length(names),1);
+            for i = 1:length(names)
+                c = find_regressors(name',names{i});
+                c = find(c);
+                disp(names{i});
+                for j = 1:length(c)
+                    fname = sprintf('beta_%3.4d.img',c(j));
+                    V = spm_vol(fullfile(EXPT.analysis_dir,S.names,M,fname));
+                    Y = spm_read_vols(V);
+                    ii = ii + 1;
+                    beta{i}(ii,:) = Y(mask);
+                end
             end
         end
     end
